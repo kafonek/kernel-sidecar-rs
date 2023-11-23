9a46bc6dcf2b270e20fb8a9e93c04d7fde226b66
kafonek <kafonek@gmail.com>
2023-11-23 08:49:15 -0500

    clean up main.rs example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use kernel_sidecar_rs::actions::Handler;
-use kernel_sidecar_rs::client::Client;
+use kernel_sidecar_rs::client::{Client, ConnectionInfo};
 use kernel_sidecar_rs::jupyter::response::Response;
-use std::error::Error;
 use std::fmt::Debug;
 use std::sync::Arc;
 
 #[derive(Debug)]
 struct DebugHandler;
+
+impl DebugHandler {
+    fn new() -> Self {
+        DebugHandler {}
+    }
+}
 
 #[async_trait::async_trait]
 impl Handler for DebugHandler {
@@ -16,15 +21,17 @@ impl Handler for DebugHandler {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    // read in file from /tmp/kernel.json
-    let connection_info = kernel_sidecar_rs::client::ConnectionInfo::from_file("/tmp/kernel.json")?;
+async fn main() {
+    // Simple example of running a Sidecar so that it connects to a running kernel,
+    // sends a kernel_info_request, and awaits until it sees the expected kernel_info_reply
+    // in addition to Kernel status going to Idle. Should print out the full parsed ZMQ messages
+    // coming back over iopub and shell channels.
+    let connection_info = ConnectionInfo::from_file("/tmp/kernel.json")
+        .expect("Make sure to run python -m ipykernel_launcher -f /tmp/kernel.json");
     let client = Client::new(connection_info).await;
 
-    // send kernel_info_request
-    let handler = DebugHandler {};
+    let handler = DebugHandler::new();
     let handlers = vec![Arc::new(handler) as Arc<dyn Handler>];
     let action = client.kernel_info_request(handlers).await;
     action.await;
-    Ok(())
 }
